91caecd1aa9f6f58baff91e77927011b63845a23
KoraKu <costhug@orange.fr>
2019-11-03 10:48:51 +0100

    Updated name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='kor',
+    name='kor_project',
     version='0.0.1',
     description="Use this package to work with kor files",
     py_modules=["kor"],
